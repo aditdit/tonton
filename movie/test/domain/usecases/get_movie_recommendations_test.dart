fd9f132e7d75b78d8521cf68aa6c9978fa336f1d
Aditia Rahmadan <tuxadit1@gmail.com>
2023-01-25 10:03:33 +0700

    [UPDATE] test

--- movie/test/domain/usecases/get_movie_recommendations_test.dart
+++ movie/test/domain/usecases/get_movie_recommendations_test.dart
@@ -1,28 +1,30 @@
 import 'package:dartz/dartz.dart';
 import 'package:core/domain/entities/movie.dart';
-import 'package:movie/domain/usecases/get_top_rated_movies.dart';
+import 'package:movie/domain/usecases/get_movie_recommendations.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 
-import '../../../core/test/helpers/test_helper.mocks.dart';
+import '../../../../core/test/helpers/test_helper.mocks.dart';
 
 void main() {
-  late GetTopRatedMovies usecase;
+  late GetMovieRecommendations usecase;
   late MockMovieRepository mockMovieRepository;
 
   setUp(() {
     mockMovieRepository = MockMovieRepository();
-    usecase = GetTopRatedMovies(mockMovieRepository);
+    usecase = GetMovieRecommendations(mockMovieRepository);
   });
 
+  const tId = 1;
   final tMovies = <Movie>[];
 
-  test('should get list of movies from repository', () async {
+  test('should get list of movie recommendations from the repository',
+      () async {
     // arrange
-    when(mockMovieRepository.getTopRatedMovies(true))
+    when(mockMovieRepository.getMovieRecommendations(tId, true))
         .thenAnswer((_) async => Right(tMovies));
     // act
-    final result = await usecase.execute(true);
+    final result = await usecase.execute(tId, true);
     // assert
     expect(result, Right(tMovies));
   });
--- movie/test/domain/usecases/get_top_rated_movies_test.dart
+++ movie/test/domain/usecases/get_top_rated_movies_test.dart
@@ -1,30 +1,28 @@
 import 'package:dartz/dartz.dart';
 import 'package:core/domain/entities/movie.dart';
-import 'package:movie/domain/usecases/get_movie_recommendations.dart';
+import 'package:movie/domain/usecases/get_top_rated_movies.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 
-import '../../../core/test/helpers/test_helper.mocks.dart';
+import '../../../../core/test/helpers/test_helper.mocks.dart';
 
 void main() {
-  late GetMovieRecommendations usecase;
+  late GetTopRatedMovies usecase;
   late MockMovieRepository mockMovieRepository;
 
   setUp(() {
     mockMovieRepository = MockMovieRepository();
-    usecase = GetMovieRecommendations(mockMovieRepository);
+    usecase = GetTopRatedMovies(mockMovieRepository);
   });
 
-  const tId = 1;
   final tMovies = <Movie>[];
 
-  test('should get list of movie recommendations from the repository',
-      () async {
+  test('should get list of movies from repository', () async {
     // arrange
-    when(mockMovieRepository.getMovieRecommendations(tId, true))
+    when(mockMovieRepository.getTopRatedMovies(true))
         .thenAnswer((_) async => Right(tMovies));
     // act
-    final result = await usecase.execute(tId, true);
+    final result = await usecase.execute(true);
     // assert
     expect(result, Right(tMovies));
   });
